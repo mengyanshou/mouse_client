8c0b5c6a5b997b715cba7563f85534ec089a78a1
nightmare <mengyanshou@gmail.com>
2023-03-13 21:50:09 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
   runApp(const MyApp());
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [],
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -29,23 +39,28 @@ class MousePage extends StatefulWidget {
 }
 
 class _MousePageState extends State<MousePage> {
-  final wsUrl = Uri.parse('ws://192.168.166.221:26541');
+  final wsUrl = Uri.parse('ws://192.168.0.101:26541');
   late WebSocketChannel channel;
+  late Socket socket;
 
   @override
   void initState() {
     super.initState();
+    connectSocket();
+  }
 
-    channel = WebSocketChannel.connect(wsUrl);
-
-    channel.stream.listen((message) {});
+  Future<void> connectSocket() async {
+    socket = await Socket.connect('192.168.0.101', 26541);
   }
 
   int pointCount = 0;
   int currentAction = -1;
-
+  late Offset pointerDownOffset;
+  final list = [];
+  Map<int, Offset> map = {};
   @override
   Widget build(BuildContext context) {
+    GestureDetector;
     return Listener(
       // onPanUpdate: (details) {
       //   channel.sink.add('mouse_move_relative:${details.delta.dx.ceil()},${details.delta.dy.ceil()}');
@@ -66,29 +81,61 @@ class _MousePageState extends State<MousePage> {
       //   // channel.sink.add('ontapup:');
       // },
       onPointerDown: (event) {
+        map[event.pointer] = event.position;
+        print('event.pointer -> ${event.pointer}');
         currentAction = 0;
         pointCount++;
-        print('pointCount -> $pointCount');
-        print('buttons -> ${event.pointer}');
+        // print('pointCount -> $pointCount');
+        // print('buttons -> ${event.pointer}');
         if (pointCount == 2) {
           currentAction = 2;
         }
-        print('onPointerDown -> $event ${event.pressure}');
-        print('currentAction -> $currentAction');
+        // print('onPointerDown -> $event ${event.pressure}');
+        // print('currentAction -> $currentAction');
       },
       onPointerMove: (event) {
-        if (currentAction == 2) return;
-        currentAction = 1;
-        print('onPointerMove -> $event ${event.pressure}');
+        if (pointCount == 2) {
+          Offset curOffset = event.position;
+          if (((map[event.pointer]! - curOffset).dy).abs() > 6) {
+            currentAction = 4;
+            socket.add([
+              4,
+              (map[event.pointer]! - curOffset).dy ~/ 6,
+            ]);
+            print('${event.pointer} onPointerMove -> ${(map[event.pointer]! - curOffset).dy}');
+            map[event.pointer] = curOffset;
+          }
+          return;
+        }
+        // print('onPointerMove -> $event ${event.pressure}');
+
+        Offset curOffset = event.position;
+        Offset diff = curOffset - map[event.pointer]!;
+        if (diff.dx.abs() > 1 || diff.dy.abs() > 1) {
+          // print('event.distance -> ${event.delta.distance}');
+          double bei = min(10, event.delta.distance);
+          bei = max(1, bei);
+          currentAction = 1;
+          socket.add([
+            1,
+            (diff.dx * bei).round(),
+            (diff.dy * bei).round(),
+          ]);
+          map[event.pointer] = curOffset;
+        }
         // channel.sink.add('mousemove:${event.position.dx.toInt()},${event.position.dy.toInt()}');
-        channel.sink.add('mouse_move_relative:${event.delta.dx.toInt()},${event.delta.dy.toInt()}');
+        // channel.sink.add('mouse_move_relative:${event.delta.dx.toInt()},${event.delta.dy.toInt()}');
       },
+      onPointerSignal: (event) {},
       onPointerUp: (event) {
         if (currentAction == 0) {
-          channel.sink.add('onlefttap:');
+          // channel.sink.add('onlefttap:');
+
+          socket.add([2]);
         } else if (currentAction == 2) {
           currentAction = -1;
-          channel.sink.add('onrighttap:');
+          socket.add([3]);
+          // channel.sink.add('onrighttap:');
         }
         pointCount--;
         print('onPointerUp -> $event ${event.pressure}');
